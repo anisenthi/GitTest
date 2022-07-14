083bbfa37c19fcee78abd0cc39902692c3c105ac
anisenthi <anithasenthil_7@rediffmail.com>
2022-07-14 13:25:13 +0530

    methods added

--- src/test/java/com/gitsample/Gitclass.java
+++ src/test/java/com/gitsample/Gitclass.java
@@ -13,5 +13,16 @@ public class Gitclass {
 		// TODO Auto-generated method stub
 
 	}
+	private void m4() {
+		// TODO Auto-generated method stub
+
+	}
+	private void m5() {
+		// TODO Auto-generated method stub
+
+	}private void m6() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
