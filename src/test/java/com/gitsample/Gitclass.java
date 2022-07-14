baa01abc517ae86da16f3ca9f50402b7dcab41e9
anisenthi <anithasenthil_7@rediffmail.com>
2022-07-14 12:59:04 +0530

    added 3 methods

--- src/test/java/com/gitsample/Gitclass.java
+++ src/test/java/com/gitsample/Gitclass.java
@@ -1,5 +1,17 @@
 package com.gitsample;
 
 public class Gitclass {
+	private void m1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void m2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void m3() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
